aad1f74d0fcf167aeb89b9deb4d4f97a74bcd616
canon1002 <k022g0038@g.neec.ac.jp>
2023-05-29 00:32:59 +0900

    プレイヤー入力処理途中

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.EventSystems.StandaloneInputModule;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -11,8 +12,11 @@ public class PlayerScript : MonoBehaviour
     public CircleCollider2D m_CircleCollider;
     public bool m_bIsMove;
 
-    private float m_fAxisHorizontal;
-    public float m_fHorizontalSpeed = 5f;
+    private CharacterController m_characterController;
+
+    // 移動量
+    public float m_fMoveSpeed = 5;
+    private Vector2 m_inputMove;
 
     // Start is called before the first frame update
     void Start()
@@ -26,27 +30,45 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
 
-        m_fAxisHorizontal = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            m_bIsMove = true;
+            transform.position.Set(m_fMoveSpeed, m_rigidbody.velocity.y, 1f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_bIsMove = true;
+        }else
+        {
+            m_bIsMove = false;
+        }
 
         // 移動のキー入力
         if (m_playerController.Player.Move.triggered)
         {
             m_bIsMove = true;
-
-            
-
         }
         else
         {
             m_bIsMove = false;
         }
 
-        // 移動処理
-        m_rigidbody.velocity = new Vector3(
-        m_fAxisHorizontal * m_fHorizontalSpeed,
-        m_rigidbody.velocity.y
-        );
+        // 操作入力と鉛直方向速度から、現在速度を計算
+        //var m_moveVelocity = new Vector3(m_inputMove.x * m_fMoveSpeed, m_rigidbody.velocity.y,m_inputMove.y * m_fMoveSpeed);
 
+        // 現在フレームの移動量を移動速度から計算
+        //var moveDelta = m_moveVelocity * Time.deltaTime;
+        //m_characterController.Move(moveDelta);
+
+        // 移動処理
+        if (m_inputMove != Vector2.zero) ;
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // 入力値を保持しておく
+        m_inputMove = context.ReadValue<Vector2>();
     }
 
     // 雪の上を歩行
